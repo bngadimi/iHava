ef7c22c315ba398cb36ee1b312b8103220c072ed
Nathaniel <nathaniel@Nelknet-Desktop.(none)>
2013-05-06 13:14:40 -0400

    Added rails files

---
+++ iHava/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IHava::Application.config.secret_token = '147d9b793c28434aad51d0831022989f415adca8996ab583422f528684a2dd31b02e76ca30fcaf08235e611ff283b880dfba5d68a0a4b7635d17d993a101e190'

b0b5fdb10f0e34b9788b41a0cf38314c33754ce4
bryan edward ngadimin <bngadimi@u.rochester.edu>
2013-05-06 15:01:30 -0400

    Adding login page with associated files, v1

---
+++ ihava.js
@@ -0,0 +1,35 @@
+document.write("<h1>");
+document.write("<i>");
+document.write("<Font Color ='Maroon'>");
+document.write("I");
+document.write("<Font Color='Maroon'>");
+document.write("H");
+document.write("<Font Color='Maroon'>");
+document.write("A");
+document.write("<Font Color='Maroon'>");
+document.write("V");
+document.write("<Font Color='Maroon'>");
+document.write("A");
+document.write("</i>");
+document.write("</Font>");
+document.write("</Font>");
+document.write("</Font>");
+document.write("</Font>");
+document.write("</Font>");
+document.write("</h1>");
+
+
+function display()
+{
+	  var textbox= document.getElementsByName("name").item(0)
+	  var value=textbox.value
+	  if(value=="")
+		  window.alert("Email does not exist or password is wrong. Password are case sensitive, please do not forget to check your CAPS key.");
+	  else
+	 	 window.alert("your username is: "+ value+"");
+	  
+}
+function nextpage()
+{
+	var 
+}

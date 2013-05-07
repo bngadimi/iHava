f84939f11161bef5243d0d71b8fcec492298461e
NatElkins <nathaniel.elkins@gmail.com>
2013-05-07 14:58:24 -0400

    Fixed some stuff

--- iHava/app/models/user.rb
+++ iHava/app/models/user.rb
@@ -11,9 +11,8 @@
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
-  has_many :collections
   has_secure_password
-  has_many :collections
+  has_many :itemgroups, dependent: :destroy
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token

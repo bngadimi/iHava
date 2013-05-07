e34767c392a5dff46b08539b52969e909816cc54
NatElkins <nathaniel.elkins@gmail.com>
2013-05-07 08:22:00 -0400

    Lots of shit

--- iHava/app/controllers/sessions_controller.rb
+++ iHava/app/controllers/sessions_controller.rb
@@ -8,8 +8,9 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
+      puts "create method in SessionsController"
       render 'new'
     end
   end
--- iHava/app/controllers/users_controller.rb
+++ iHava/app/controllers/users_controller.rb
@@ -11,6 +11,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def index
+    @users = User.all
+  end
+
+
   def display_collection
   end
 
@@ -39,7 +44,10 @@ class UsersController < ApplicationController
   private
 
     def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
     end
 
     def correct_user
